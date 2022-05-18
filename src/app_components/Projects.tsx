a13ad5ccde6ce18be5f1b93578a324eed6490aed
vileider <andrewbagio@yahoo.com>
2022-05-18 19:50:44 +0100

    Projects style update

--- src/app_components/Projects.tsx
+++ src/app_components/Projects.tsx
@@ -6,12 +6,17 @@ import '../reusable_styles/BackgroundGlow.css'
 export function Projects() {
 
 const body = (
-	<div className="projectsBody">
-		<div className="arrow left "></div>
-		<div className="project backgroundGlow"> 1st</div>
-		<div className="project backgroundGlow"> 2nd</div>
-		<div className="project backgroundGlow"> 3rd</div>
-		<div className="arrow right"></div>
+	<div className="projectBody">
+		<div className="projectTitle">
+		OUR PROJECTS
+			</div> 
+		<div className="projectsSlider">
+			<div className="arrow left "></div>
+			<div className="project backgroundGlow"> 1st</div>
+			<div className="project backgroundGlow"> 2nd</div>
+			<div className="project backgroundGlow"> 3rd</div>
+			<div className="arrow right"></div>
+		</div>
 	</div>
 
 )
