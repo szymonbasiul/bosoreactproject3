89794526584260caab9545065e5d315393310037
vileider <andrewbagio@yahoo.com>
2022-05-18 12:49:03 +0100

    update: * project section * blur color at color palette small fixes

--- src/app_components/Projects.tsx
+++ src/app_components/Projects.tsx
@@ -7,9 +7,11 @@ export function Projects() {
 
 const body = (
 	<div className="projectsBody">
-		<div className="project BackgroundGlow"> 1st</div>
-		<div className="project BackgroundGlow"> 1st</div>
-		<div className="project BackgroundGlow"> 1st</div>
+		<div className="arrow left "></div>
+		<div className="project backgroundGlow"> 1st</div>
+		<div className="project backgroundGlow"> 2nd</div>
+		<div className="project backgroundGlow"> 3rd</div>
+		<div className="arrow right"></div>
 	</div>
 
 )
