9ac7a406280154012f6b1b34ec2281bbe220b280
szymo <szymon.basiul@wp.pl>
2022-05-18 21:51:08 +0200

    First version of footer component.  Searching for a way to make icons customizable.

--- src/app_components/Footer.tsx
+++ src/app_components/Footer.tsx
@@ -1,3 +1,25 @@
+import "./styles/Footer.css";
+
 export function Footer() {
-	return <div>Footer</div>;
+	return (
+		<div className="footerSection">
+			<div className="footerIcons">
+				<div className="socialIcon">
+					<div className="fa fa-facebook"></div>
+				</div>
+				<div className="socialIcon">
+					<img
+						alt="linkedin"
+						src="https://img.icons8.com/glyph-neue/100/000000/linkedin-circled.png"
+					/>
+				</div>
+				<div className="socialIcon">
+					<img
+						alt="whatsapp"
+						src="https://img.icons8.com/ios-filled/100/000000/whatsapp--v1.png"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
