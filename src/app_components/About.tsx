578bab9b2ba416b73918c3214fe2d8feed6fea6e
vileider <andrewbagio@yahoo.com>
2022-05-24 21:16:43 +0100

    adjusting main page to figma pattern

--- src/app_components/About.tsx
+++ src/app_components/About.tsx
@@ -3,10 +3,11 @@ import "../reusable_styles/BackgroundGlow.css";
 
 export function About() {
 	const header = (
-		<>
+		<><div className="aboutTitle">The Project Of</div>
 			<div className="about">
 				<div className="backgroundGlow">
-					<div className="aboutContent"></div>
+					<div className="aboutContent">	
+					</div>
 				</div>
 			</div>
 		</>
