25a4fdb5152b3b32bee9b49c5a6dd7b61fb6a86f
vileider <andrewbagio@yahoo.com>
2022-06-07 19:38:54 +0100

    update

--- src/app_components/About.tsx
+++ src/app_components/About.tsx
@@ -1,57 +1,56 @@
 import "./styles/About.css";
 import "../reusable_styles/BackgroundGlow.css";
-import { useState, FunctionComponent } from "react";
+import  {useState, FunctionComponent } from "react";
+
 
 interface participantsDefinition {
 	map(x: (x: string) => JSX.Element): JSX.Element[];
-	[index: number]: string;
+	[index:number] : string
 }
 
 interface activePeopleDefinition {
-	[key: string]: boolean;
+[key:string]: boolean	
 }
 
 const activePeopleProto: activePeopleDefinition = {
-	timi: false,
-	simon: false,
-	bogdan: false,
-};
+	"timi" : false,
+	"simon" : false,
+	"bogdan" : false
+}
 
-export const About: FunctionComponent = () => {
-	const [activePeople, setActivePeople] = useState(activePeopleProto);
+export const About: FunctionComponent= () => {
 
-	const participants: participantsDefinition = ["timi", "simon", "bogdan"];
+	const [activePeople, setActivePeople] = useState(activePeopleProto)
 
-	const personToggle = (person: string) => {
+	const participants: participantsDefinition = ["timi", "simon", "bogdan"]
+
+	const personToggle = (person : string) => {
 		const tempObj = activePeopleProto;
 		tempObj[person] = true;
-		console.log(tempObj);
-	};
-	personToggle("timi");
-	const peopleComponent: JSX.Element[] = participants.map((x) => {
-		return (
+		console.log(tempObj)
+	}
+	personToggle("timi")
+	const peopleComponent: JSX.Element[] = participants.map( x => {
+		return(
 			<div className="person" key={x}>
-				<div
-					className="photo"
-					onMouseEnter={() => {
-						console.log(x);
-					}}
-				></div>
-				<div className="description">Lorem ipsum dolor sit amet.</div>
-			</div>
-		);
-	});
+							<div className="photo" onMouseEnter={()=> {console.log(x)}}></div>
+							<div className="description">Lorem ipsum dolor sit amet.</div>
+						</div>
+		)
+	})
 
 	const header = (
 		<>
 			<div className="aboutBody">
 				<div className="aboutTitle">The Project Of</div>
 				<div className="backgroundGlow">
-					<div className="aboutContent">{peopleComponent}</div>
+					<div className="aboutContent">
+						{peopleComponent}
+					</div>
 				</div>
 			</div>
 		</>
 	);
 
 	return header;
-};
+}
