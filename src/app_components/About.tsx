2dc07ed1293d26509e980fbd8d5b650057d3f368
szymo <szymon.basiul@wp.pl>
2022-06-07 22:00:24 +0200

    Testing class implementation.

--- src/app_components/About.tsx
+++ src/app_components/About.tsx
@@ -1,56 +1,71 @@
 import "./styles/About.css";
 import "../reusable_styles/BackgroundGlow.css";
-import  {useState, FunctionComponent } from "react";
-
+import { useState, FunctionComponent } from "react";
 
 interface participantsDefinition {
 	map(x: (x: string) => JSX.Element): JSX.Element[];
-	[index:number] : string
+	[index: number]: string;
 }
 
 interface activePeopleDefinition {
-[key:string]: boolean	
+	[key: string]: boolean;
 }
 
-const activePeopleProto: activePeopleDefinition = {
-	"timi" : false,
-	"simon" : false,
-	"bogdan" : false
-}
+// class PeopleObjectTest {
+// 	"timi" = false;
+// 	"simon" = false;
+// 	"bogdan" = false;
+// }
 
-export const About: FunctionComponent= () => {
+class activePeopleProto {
+	"timi" = false;
+	"simon" = false;
+	"bogdan" = false;
 
-	const [activePeople, setActivePeople] = useState(activePeopleProto)
-
-	const participants: participantsDefinition = ["timi", "simon", "bogdan"]
-
-	const personToggle = (person : string) => {
-		const tempObj = activePeopleProto;
-		tempObj[person] = true;
-		console.log(tempObj)
+	constructor() {
+		this["timi"] = false;
+		this["simon"] = false;
+		this["bogdan"] = false;
 	}
-	personToggle("timi")
-	const peopleComponent: JSX.Element[] = participants.map( x => {
-		return(
+}
+
+export const About: FunctionComponent = () => {
+	const [activePeople, setActivePeople] = useState(activePeopleProto);
+
+	const participants: participantsDefinition = ["timi", "simon", "bogdan"];
+
+	const personToggle = (person: string) => {
+		const tempObj = new activePeopleProto();
+		// tempObj[person] = true;
+		// setActivePeople(tempObj);
+		console.log(tempObj);
+	};
+
+	// personToggle("timi")
+	const peopleComponent: JSX.Element[] = participants.map((x) => {
+		return (
 			<div className="person" key={x}>
-							<div className="photo" onMouseEnter={()=> {console.log(x)}}></div>
-							<div className="description">Lorem ipsum dolor sit amet.</div>
-						</div>
-		)
-	})
+				<div
+					className="photo"
+					onMouseEnter={() => {
+						personToggle(x);
+					}}
+				></div>
+				<div className="description">Lorem ipsum dolor sit amet.</div>
+			</div>
+		);
+	});
 
 	const header = (
 		<>
 			<div className="aboutBody">
 				<div className="aboutTitle">The Project Of</div>
 				<div className="backgroundGlow">
-					<div className="aboutContent">
-						{peopleComponent}
-					</div>
+					<div className="aboutContent">{peopleComponent}</div>
 				</div>
 			</div>
 		</>
 	);
 
 	return header;
-}
+};
